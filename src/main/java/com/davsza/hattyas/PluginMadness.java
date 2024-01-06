cc5dc8ed4300b3893d46b9798645b69b49ab86a9
davsza <david.szabolcs115@gmail.com>
2024-01-06 15:06:01 +0100

    Init

---
+++ src/main/java/com/davsza/hattyas/PluginMadness.java
@@ -0,0 +1,17 @@
+package com.davsza.hattyas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginMadness extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
